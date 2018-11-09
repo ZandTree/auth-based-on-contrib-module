10d5dea5b063884658d31510b5f9e432a26ef926
Tanja Rietveldt <diafox2015@gmail.com>
2018-11-09 20:10:01 +0100

    start home page and menu

--- authcontr/authcontr/urls.py
+++ authcontr/authcontr/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views import generic
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',generic.TemplateView.as_view(template_name='layout.html'),name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
